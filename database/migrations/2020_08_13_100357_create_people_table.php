01cff8d139d6d20c5424ef247f2bd2c28969c498
Eliza Ahmed <elizarafee@gmail.com>
2020-08-13 16:11:32 +0600

    model and table created for people

---
+++ database/migrations/2020_08_13_100357_create_people_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 30);
+            $table->string('age', 3);
+            $table->string('city', 30);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->string('deleted_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('people', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+    }
+}
